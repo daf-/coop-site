a4c0cd4e6808caf7aa13538143b6f5bbde15f3c6
Jack Ratner <jackratner@gmail.com>
2013-10-13 12:30:39 -0400

    create a default rails app for us to build off of!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoopSite::Application.config.secret_key_base = '05e349c6f463320d78fad7d45515f5f805d5a75b34d1bfd96d0dfc00b6edac98bdac7ee2aca916ed0c1cb3e7341ef95da22506339fbea4fdb1670b39827a3ba1'

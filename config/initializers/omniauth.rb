6057ea016a8c4cf7ef1b1b59427f784611ed2464
Jack Ratner <jackratner@gmail.com>
2013-10-15 17:26:24 -0400

    some logic for omniauth(commented out)

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV["950188685519.apps.googleusercontent.com"], ENV["Ky_jXimiE8fsRBSGxPjLBc3e"],
+#   	{
+#   		:scope => "userinfo.email"
+#   	}
+# end

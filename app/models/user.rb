9e0474a4d9276764022d61728c1479811af55af7
Dan Friedman <dfriedma@oberlin.edu>
2013-10-29 17:26:28 -0400

    Fix indentation in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-	validates_uniqueness_of :email
+  validates_uniqueness_of :email
 
-	def coopName
-		if self.coop_id
-			Coop.find(self.coop_id).name
-		end
-	end
+  def coopName
+    if self.coop_id
+      Coop.find(self.coop_id).name
+    end
+  end
 
-	def coopName=(name)
+  def coopName=(name)
     @coop = Coop.find_or_create_by name: name
     self.coop_id = @coop.id
   end

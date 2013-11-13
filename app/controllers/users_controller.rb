46f1d4cf31b0f9be9cf9a5e76e4bde48517d592a
jackrr <jackratner@gmail.com>
2013-11-13 15:08:20 -0500

    force user to fill in a username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
+      if !user_params[:username] || user_params[:username] == ''
+        format.html { redirect_to edit_user_path(@user), notice: 'You must have a username!' }
+        format.json { head :no_content }
+      end
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }

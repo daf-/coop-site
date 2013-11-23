777391379845da5885fa3d01dce8f1aacf3f1d22
jackrr <jackratner@gmail.com>
2013-11-23 13:16:55 -0500

    basic design improvements of personal shift area

--- app/assets/javascripts/shifts.js
+++ app/assets/javascripts/shifts.js
@@ -1,13 +1,13 @@
 
 var ready = function() {
-  $('.shift_header .name').click(function(e) {
-    $(e.currentTarget).parent().siblings('.shift_info').toggle();
+  $('.shift_header .left_content').click(function(e) {
+    $(e.currentTarget).closest('.shift_header').siblings('.shift_info').toggle();
   });
 
   $('.shift .remove_shift_link').click(function(e) {
   	e.preventDefault();
   	$.get($(e.currentTarget).attr('href'), function(res) {
-  		$(e.currentTarget).parent().remove();
+  		$(e.currentTarget).closest('.shift').remove();
   	});
   });
 };

351839cba0a33743ee6f8c23332206a89f3829dd
jackrr <jackratner@gmail.com>
2013-11-24 12:11:21 -0500

    fix email urls

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url = 'http://localhost:3000'
+    @url = 'http://radradishes.cs.oberlin.edu'
     mail(to: @user.email, subject: "Welcome to the coop hub site thing")
   end
 
   def coop_join_info_email(user, coop)
     @user = user
     @coop = coop
-    @url = "http://localhost:3000/coops/#{coop.id}/member_join_link/#{coop.member_join_hash}"
-    mail(to: @user.email, subject: "#{@coop.name} member join link!")
+    @url = "http://radradishes.cs.oberlin.edu/coops/#{coop.id}/member_join_link/#{coop.member_join_hash}"
+    mail(to: @user.email, subject: "Join #{@coop.name} as a member!")
   end
 
   def coop_admin_email(user, coop)
     @user = user
     @coop = coop
-    @url = "http://localhost:3000/coops/#{coop.id}/admin_join_link/#{coop.admin_join_hash}"
-    mail(to: @user.email, subject: "#{@coop.name} admin join link!")
+    @url = "http://radradishes.cs.oberlin.edu/coops/#{coop.id}/admin_join_link/#{coop.admin_join_hash}"
+    mail(to: @user.email, subject: "Join #{@coop.name} as an admin!")
   end
 end
